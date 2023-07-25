53d0dcffa3556c0a4d8da54b957e8ca5cc13a908
suguoxu <1573591859@qq.com>
2023-07-25 22:29:11 +0800

    首次添加，增加tty杂项

---
+++ tty杂项/初始化相关.c
@@ -0,0 +1,21 @@
+pty.c
+    static void __init legacy_pty_init(void)
+    static void __init unix98_pty_init(void)
+    static int __init pty_init(void)
+sysrq.c
+    static int __init sysrq_always_enabled_setup(char *str)
+    static int __init sysrq_init(void)
+tty_io.c
+    void __init console_init(void)
+    static int __init tty_class_init(void)
+    int __init tty_init(void)
+vt/consolemap.c
+    void __init console_map_init(void)
+vt/keyboard.c
+    int __init kbd_init(void)
+vt/vc_screen.c
+    int __init vcs_init(void)
+vt/vt.c
+    static int __init con_init(void)
+    int __init vty_init(const struct file_operations *console_fops)
+    static int __init vtconsole_class_init(void)
---
+++ tty杂项/涉及到的文件.c
@@ -0,0 +1,22 @@
+suguoxu@sgx:~/linux/my/pro/bsp/kernel/linux/drivers/tty$ tree
+├── n_tty.c
+├── pty.c
+├── serial/
+├── sysrq.c
+├── tty_buffer.c
+├── tty_io.c
+├── tty_ioctl.c
+├── tty_ldisc.c
+├── tty_ldsem.c
+├── tty_mutex.c
+├── tty_port.c
+└── vt/
+    ├── consolemap.c
+    ├── keyboard.c
+    ├── selection.c
+    ├── vc_screen.c
+    ├── vt.c
+    └── vt_ioctl.c
+
+7 directories, 214 files
+suguoxu@sgx:~/linux/my/pro/bsp/kernel/linux/drivers/tty$
