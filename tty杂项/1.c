5eb7eea8274f5387f23d39d6a91f90e434247deb
suguoxu <1573591859@qq.com>
2023-08-13 23:19:52 +0800

    增加tty.pptx的终端内容

--- tty杂项/1.c
+++ tty杂项/1.c
@@ -1,1 +1,16 @@
-#define this_cpu_read(pcp)      __pcpu_size_call_return(this_cpu_read_, pcp)
+https://www.cnblogs.com/sparkdev/p/11460821.html
+https://www.cnblogs.com/sparkdev/p/11605804.html
+https://encyclopedia.thefreedictionary.com/Computer+terminal
+https://encyclopedia.thefreedictionary.com/pseudo+terminal.
+https://www.cnblogs.com/aozhejin/p/16065019.html
+https://blog.csdn.net/qq_39785418/article/details/127176522
+http://www.360doc.com/content/18/0815/12/54508727_778436804.shtml
+https://www.cnblogs.com/hnrainll/archive/2011/12/13/2285866.html
+http://www.wowotech.net/tty_framework/application_view.html
+
+命令
+    tty     通过tty命令可以知道当前具体是什么终端（所以tty也代表当前的终端，echo hello > /dev/tty都会显示在当前的终端中，可能是上面的任意一种终端）。
+    toe -a  查看系统支持的终端类型
+    who     查看当前连接计算机的终端设备
+    w       查看当前正在连接的终端设备和正在使用的程序
+    ps -ax  查看进程与哪个控制终端相连(如果当前进程有控制终端(Controlling Terminal)的话)
